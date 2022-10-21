8565fa72ca618b14d326dc609a9366a6112b62d5
Kirill Levin <levinkirill@yandex.ru>
2022-10-21 14:52:46 +0000

    Sync LeetCode submission - Valid Anagram (python3)

---
+++ problems/valid_anagram/solution.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        s_dict = {}
+        t_dict = {}
+        
+        for letter in s:
+            if letter not in s_dict:
+                s_dict[letter] = 1
+            else:
+                s_dict[letter] += 1
+
+        for letter in t:
+            if letter not in t_dict:
+                t_dict[letter] = 1
+            else:
+                t_dict[letter] += 1
+        
+        return True if s_dict == t_dict else False
