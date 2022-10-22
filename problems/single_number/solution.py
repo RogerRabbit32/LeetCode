97891f2f6c24b0fa3d49827fd85e6b3315f0203e
Kirill Levin <levinkirill@yandex.ru>
2022-10-22 14:50:42 +0000

    Sync LeetCode submission - Single Number (python3)

---
+++ problems/single_number/solution.py
@@ -0,0 +1,12 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        count = {}
+        for i in nums:
+            if i not in count:
+                count[i] = 1
+            else:
+                count[i] += 1
+        for num in count:
+            if count[num] == 1:
+                return num
+        
